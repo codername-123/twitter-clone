d073cf0a7e2c8681204560d9c3d21147c7a1adf6
codername-132 <nitin.rawat629@gmail.com>
2023-07-04 11:14:10 +0530

    CleanUp: no need to handle omitting some field from user as it is already handled in user services

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,16 +1,12 @@
 import { createUser } from "../services/user.service.js";
 import { StatusCodes } from "http-status-codes";
-import lodash from "lodash";
 import logger from "../utils/winston.js";
-import { privateFields } from "../model/user.model.js";
 
 export async function createUserHandler(req, res) {
   try {
     const body = req.body;
     const user = await createUser(body);
-    return res
-      .status(StatusCodes.CREATED)
-      .json(lodash.omit(user.toJSON(), privateFields));
+    return res.status(StatusCodes.CREATED).json(user);
   } catch (error) {
     logger.debug(error);
     if (error.code === 11000) {
